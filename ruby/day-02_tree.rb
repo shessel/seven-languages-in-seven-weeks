a8c12966b49ce6113ed38bd21e4e5bb07db171df
shessel <shessel@users.noreply.github.com>
2017-01-21 22:40:04 +0100

    ruby day 2 create a tree from a nested hash

---
+++ ruby/day-02_tree.rb
@@ -0,0 +1,26 @@
+class Tree
+  attr_accessor :children, :value
+
+  def initialize(hash)
+    if hash.keys.size > 1
+      puts "wrong structure, can't have more than one value per node"
+    end
+    @value = hash.keys[0]
+    @children = []
+    hash[@value].each do |value, children|
+      @children.push(Tree.new({value => children}))
+    end
+  end
+
+  def visit(&block)
+    block.call self
+  end
+
+  def visit_all(&block)
+    visit(&block)
+    children.each {|child| child.visit_all(&block)}
+  end
+end
+
+tree = Tree.new({:a => {:b1 => {:c1 => {}, :c2 => {}}, :b2 => {}}})
+tree.visit_all {|node| puts node.value}
